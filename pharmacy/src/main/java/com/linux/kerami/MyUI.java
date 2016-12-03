bab1b676493fc7e21241cc11e92f18574b145608
Kerami Ozsoy <keramiozsoy@gmail.com>
2016-12-03 02:55:57 +0200

    Added first button end textfield

--- pharmacy/src/main/java/com/linux/kerami/MyUI.java
+++ pharmacy/src/main/java/com/linux/kerami/MyUI.java
@@ -10,6 +10,8 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -22,18 +24,44 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
+//        final VerticalLayout layout = new VerticalLayout();
+//        layout.setMargin(true);
+//        setContent(layout);
+//
+//        Button button = new Button("Click Me");
+//        Button button2 = new Button("Kenk");
+//        button.addClickListener(new Button.ClickListener() {
+//            @Override
+//            public void buttonClick(ClickEvent event) {
+//                layout.addComponent(new Label("Thank you for clicking kanak"));
+//            }
+//        });
+//        layout.addComponent(button);
+//        layout.addComponent(button2);
+    	
+    	 
+    	Label label  = new Label(" Hello this is my application .....");
 
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
-        });
-        layout.addComponent(button);
+    	final TextField txtfield = new TextField("Enter something");
+    	
+    	Button button = new Button("First You should write and click");
+    	button.addClickListener(new Button.ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				Notification.show("The caption",
+		                  "This is " + txtfield.getValue(),
+		                  Notification.Type.ERROR_MESSAGE);
+				
+			}
+		});
+    	
+    	VerticalLayout vl  = new VerticalLayout();
+    	vl.setSpacing(true);
+    	vl.setMargin(true);
+    	vl.addComponent(label);
+    	vl.addComponents(txtfield,button);
+    	setContent(vl);
 
     }
 
